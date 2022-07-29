e15cb88aee5de12593bf75476685061991485852
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2022-07-29 10:31:04 +0530

    Clear before flutter_isolate

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,12 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:sound_mode/permission_handler.dart';
 import 'package:sound_mode/sound_mode.dart';
 import 'package:sound_mode/utils/ringer_mode_statuses.dart';
 import 'package:toner/constants/themes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 
 bool darkMode = false;
 bool button = false;
@@ -27,109 +26,7 @@ Future<void> main() async {
     // Opens the Do Not Disturb Access settings to grant the access
     await PermissionHandler.openDoNotDisturbSetting();
   }
-  await initializeService();
   runApp(const MyApp());
-}
-
-Future<void> initializeService() async {
-  final service = FlutterBackgroundService();
-  await service.configure(
-    androidConfiguration: AndroidConfiguration(
-      // this will be executed when app is in foreground or background in separated isolate
-      onStart: onStart,
-
-      // auto start service
-      autoStart: true,
-      isForegroundMode: true,
-    ),
-    iosConfiguration: IosConfiguration(
-      // auto start service
-      autoStart: true,
-
-      // this will be executed when app is in foreground in separated isolate
-      onForeground: onStart,
-
-      // you have to enable background fetch capability on xcode project
-      onBackground: onIosBackground,
-    ),
-  );
-  service.startService();
-}
-
-bool onIosBackground(ServiceInstance service) {
-  WidgetsFlutterBinding.ensureInitialized();
-  print('FLUTTER BACKGROUND FETCH');
-
-  return true;
-}
-
-void onStart(ServiceInstance service) async {
-  // Only available for flutter 3.0.0 and later
-  DartPluginRegistrant.ensureInitialized();
-
-  // For flutter prior to version 3.0.0
-  // We have to register the plugin manually
-
-  SharedPreferences preferences = await SharedPreferences.getInstance();
-  await preferences.setString("hello", "world");
-  preferences.reload();
-
-  if (service is AndroidServiceInstance) {
-    service.on('setAsForeground').listen((event) {
-      service.setAsForegroundService();
-    });
-
-    service.on('setAsBackground').listen((event) {
-      service.setAsBackgroundService();
-    });
-  }
-
-  service.on('stopService').listen((event) {
-    service.stopSelf();
-  });
-
-  // bring to foreground
-  Timer.periodic(const Duration(seconds: 1), (timer) async {
-    String ringerStatus = (await SoundMode.ringerModeStatus).toString();
-
-    bool? fetchButtonBit = preferences.getBool("button");
-    print(fetchButtonBit);
-
-    fetchButtonBit ??= false;
-
-    var curTime = DateTime.now();
-
-    if (service is AndroidServiceInstance) {
-      service.setForegroundNotificationInfo(
-        title: "My App Service",
-        content: "${fetchButtonBit} and ${curTime}",
-      );
-    }
-
-    /// you can see this log in logcat
-    // print('FLUTTER BACKGROUND SERVICE: ${DateTime.now()}');
-
-    if (fetchButtonBit) {
-      if (curTime.hour >= 17) {
-        print("time to change");
-        try {
-          await SoundMode.setSoundMode(RingerModeStatus.silent);
-        } on PlatformException {
-          print('Please enable permissions required');
-        }
-      }
-    }
-
-    // test using external plugin
-
-    service.invoke(
-      'update',
-      {
-        "current_date": DateTime.now().toIso8601String(),
-        // "device": device,
-      },
-    );
-  });
 }
 
 class MyApp extends StatelessWidget {
